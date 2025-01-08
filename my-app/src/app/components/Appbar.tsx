6fcad5da14569fdbbfd1da6d6bf5fa6082e1e5bb
Matthewkuria <mathewkwachira@gmail.com>
2025-01-08 23:54:40 +0300

    add the navbar

---
+++ my-app/src/app/components/Appbar.tsx
@@ -0,0 +1,54 @@
+"use client"
+import { Navbar, NavbarContent, NavbarMenuToggle, NavbarBrand, NavbarItem, Button, NavbarMenu, NavbarMenuItem } from '@nextui-org/react'
+import Link from 'next/link'
+import React from 'react'
+
+const Appbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  return (
+    <Navbar className='shadow-md' onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link aria-current="page" href="#">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        
+      </NavbarMenu>
+    </Navbar>
+  )
+}
+
+export default Appbar
