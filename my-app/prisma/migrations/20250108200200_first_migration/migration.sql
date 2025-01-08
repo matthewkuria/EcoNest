a719b5a2f50bb4deaac900aa4aa3cb6fb351f8f7
Matthewkuria <mathewkwachira@gmail.com>
2025-01-08 23:02:56 +0300

    Add prisma and migrations

---
+++ my-app/prisma/migrations/20250108200200_first_migration/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "avatarUrl" TEXT,
+    "createAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
