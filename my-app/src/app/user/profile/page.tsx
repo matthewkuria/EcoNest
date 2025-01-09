2f9cd6945b58bfdb7ea4c6bcef8a047e0cae160d
Matthewkuria <mathewkwachira@gmail.com>
2025-01-09 23:05:50 +0300

    Add profile avatar from the nextui

--- my-app/src/app/user/profile/page.tsx
+++ my-app/src/app/user/profile/page.tsx
@@ -1,8 +1,9 @@
 import PageTitle from '@/app/components/PageTitle';
 import { getUserById } from '@/app/lib/actions/user';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { Card } from '@nextui-org/react';
-import React from 'react'
+import { Avatar, Card } from '@nextui-org/react';
+import React, { ReactNode } from 'react'
+import SectionTitle from './_components/sectionTitle';
 
 const ProfilePage = async () => {
     const { getUser } = await getKindeServerSession();
@@ -11,12 +12,28 @@ const ProfilePage = async () => {
   return (
       <div>
           <PageTitle title='My Profile' linkCaption='Back to Home Page' href='/' />
-          <Card>
-              <h2>{dbUser?.firstName} {dbUser?.lastName}</h2>
-              <p>{dbUser?.email}</p>
+          <Card className='p-4 m-4'>
+              <SectionTitle title='Profile Information' />
+              <div className="">
+                  <Avatar src={dbUser?.avatarUrl ?? '/avatar.png'}  />
+              </div>
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                  <Attribute title='Name' value={`${dbUser?.firstName} ${dbUser?.lastName}`} />
+                  <Attribute title='Email' value={dbUser?.email ?? 'N/A'} />
+                  <Attribute title='Registered On' value={dbUser?.createAt.toLocaleDateString() ?? 'N/A'} />
+                  <Attribute title='Properties Posted' value={1} />
+              </div>
         </Card> 
     </div>
   )
 }
 
 export default ProfilePage
+
+
+const Attribute = ({ title, value }: { title: string; value: ReactNode }) => ( 
+    <div className=" flex flex-col text-sm">
+        <span className="text-slate-800 font-semibold">{title }</span>
+        <span className="text-slate-600">{value }</span>
+    </div>
+)
